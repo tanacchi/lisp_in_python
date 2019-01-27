bb4ecf09f83a494494e2fb9a8da6cc74f7341019
tanacchi <q111026d@mail.kyutech.jp>
2019-01-27 21:44:00 +0900

    Add minus evaluation test and source

--- python/lisp/lisp01.py
+++ python/lisp/lisp01.py
@@ -1,5 +1,11 @@
 def evaluate(arg_list):
-    arg = 0
-    for elem in arg_list[1:]:
-        arg += elem
-    return arg
+    ans = 0
+    if arg_list[0] == '+':
+        for elem in arg_list[1:]:
+            ans += elem
+        return ans
+    if arg_list[0] == '-':
+        ans += arg_list[1]
+        for elem in arg_list[2:]:
+            ans -= elem
+        return ans
--- python/tests/test_lisp01.py
+++ python/tests/test_lisp01.py
@@ -3,14 +3,23 @@ from lisp import lisp01
 
 
 class TestLisp01(TestCase):
-    def test_evaluator01(self):
+    def test_plus_evaluator01(self):
         eval_args = ['+', 1]
         self.assertEqual(lisp01.evaluate(eval_args), 1)
 
-    def test_evaluator02(self):
+    def test_plus_evaluator02(self):
         eval_args = ['+', 1, 2]
         self.assertEqual(lisp01.evaluate(eval_args), 1+2)
 
-    def test_evaluator03(self):
+    def test_plus_evaluator03(self):
         eval_args = ['+', 1, 2, 3]
         self.assertEqual(lisp01.evaluate(eval_args), 1+2+3)
+
+    def test_minus_evaluator01(self):
+        eval_args = ['-', 1, 2]
+        self.assertEqual(lisp01.evaluate(eval_args), 1-2)
+
+    def test_minus_evaluator02(self):
+        eval_args = ['-', 5, 3, 3]
+        self.assertEqual(lisp01.evaluate(eval_args), 5-3-3)
+
