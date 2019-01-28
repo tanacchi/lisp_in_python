e40de0ae556c1ef676f17bbbfe6062c8b58d8de7
tanacchi <q111026d@mail.kyutech.jp>
2019-01-29 02:19:03 +0900

    Add code and test for evaluation of div

--- python/lisp/lisp01.py
+++ python/lisp/lisp01.py
@@ -13,10 +13,17 @@ def multi(args):
         ans *= elem
     return ans
 
+def div(args):
+    ans = args[0]
+    for elem in args[1:]:
+        ans /= elem
+    return ans
+
 def evaluate(arg_list):
     dispatch_table = {
         '+': add,
         '-': minus,
-        '*': multi 
+        '*': multi,
+        '/': div
     }
     return dispatch_table.get(arg_list[0])(arg_list[1:])
