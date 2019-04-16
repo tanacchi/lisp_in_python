e97683917ca603283445be879c0187edca36338e
tanacchi <q111026d@mail.kyutech.jp>
2019-04-17 02:07:09 +0900

    Upgrade lisp03 to complete test

--- python/src/lisp03.py
+++ python/src/lisp03.py
@@ -1,19 +1,31 @@
+import re
+
 class Reader(object):
     def __init__(self):
         self.__data = []
 
     def read(self, src):
-        for ch in src:
+        index, end = 0, len(src)
+        while index < end:
+            match = re.search(re.compile(r'[\w\d]+'), src[index:])
+            if match != None and match.span()[0] == 0:
+                detected_word = match.group(0)
+                self.__data.append(detected_word)
+                index += len(detected_word)
+
+            ch = src[index]
             if ch == " ":
-                continue
+                index += 1
             elif ch == "(":
-                pass
+                index += 1
             elif ch == ")":
-                pass
-            else:
-                self.__data.append(ch)
-        return self.__data;
+                index += 1
+
+        return self.__data
 
 
 if __name__ == '__main__':
-    pass
+    source_str = "( Hello World)"
+    reader = Reader()
+    result = reader.read(source_str)
+    print(result)
--- python/tests/test_lisp03.py
+++ python/tests/test_lisp03.py
@@ -9,3 +9,7 @@ class TestLisp01(TestCase):
     def test_simple_reader(self):
         result = self.reader.read("( 1 2 3 )");
         self.assertEqual(result, ["1", "2", "3"]);
+
+    def test_strings_list_reader(self):
+        result = self.reader.read("(hello world)");
+        self.assertEqual(result, ["hello", "world"]);
