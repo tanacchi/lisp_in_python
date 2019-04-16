5dd255fa0c4b7d5786fd2afc57f8a389b7a59ec5
tanacchi <q111026d@mail.kyutech.jp>
2019-04-17 02:43:40 +0900

    Add test for nested list

--- python/tests/test_lisp03.py
+++ python/tests/test_lisp03.py
@@ -7,9 +7,13 @@ class TestLisp01(TestCase):
         self.reader = lisp03.Reader()
 
     def test_simple_reader(self):
-        result = self.reader.read("( 1 2 3 )");
-        self.assertEqual(result, ["1", "2", "3"]);
+        result = self.reader.read("( 1 2 3 )")
+        self.assertEqual(result, ["1", "2", "3"])
 
     def test_strings_list_reader(self):
-        result = self.reader.read("(hello world)");
-        self.assertEqual(result, ["hello", "world"]);
+        result = self.reader.read("(hello world)")
+        self.assertEqual(result, ["hello", "world"])
+
+    def test_nested_list_reader(self):
+        result = self.reader.read("( ABC (DEF GHI) JKL )")
+        self.assertEqual(result, ["ABC", ["DEF", "GHI"], "JKL"])
