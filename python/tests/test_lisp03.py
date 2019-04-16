b124ac1cf1f4d49640b7333b431a916b61c3e334
tanacchi <q111026d@mail.kyutech.jp>
2019-04-16 23:54:20 +0900

    Add minimum test of lisp03

--- python/tests/test_lisp03.py
+++ python/tests/test_lisp03.py
@@ -4,4 +4,8 @@ from src import lisp03
 
 class TestLisp01(TestCase):
     def setUp(self):
-        pass
+        self.reader = lisp03.Reader()
+
+    def test_simple_simple_reader(self):
+        result = self.reader.read("( 1 2 3 )");
+        self.assertEqual(result, ["1", "2", "3"]);
