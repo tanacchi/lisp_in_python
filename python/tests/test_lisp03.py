4173029d9da5523e670a3d513261efad79bb275d
tanacchi <q111026d@mail.kyutech.jp>
2019-04-17 01:07:15 +0900

    Fix test name for lisp03

--- python/tests/test_lisp03.py
+++ python/tests/test_lisp03.py
@@ -6,6 +6,6 @@ class TestLisp01(TestCase):
     def setUp(self):
         self.reader = lisp03.Reader()
 
-    def test_simple_simple_reader(self):
+    def test_simple_reader(self):
         result = self.reader.read("( 1 2 3 )");
         self.assertEqual(result, ["1", "2", "3"]);
