9878f769f4af31ca808d45278735059f15ab2c98
tanacchi <q111026d@mail.kyutech.jp>
2019-04-17 23:28:49 +0900

    Refactor word finder

--- python/src/lisp03.py
+++ python/src/lisp03.py
@@ -7,8 +7,8 @@ class Reader(object):
     def read(self, src):
         index, end = 0, len(src)
         while index < end:
-            match = re.search(re.compile(r'[\w\d]+'), src[index:])
-            if match != None and match.span()[0] == 0:
+            match = re.search(re.compile(r'^[\w\d]+'), src[index:])
+            if match != None:
                 detected_word = match.group(0)
                 self.__data.append(detected_word)
                 index += len(detected_word)
