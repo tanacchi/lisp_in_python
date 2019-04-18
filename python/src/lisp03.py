1a6767186b22d0af853004ec21a60479bf1c5680
tanacchi <q111026d@mail.kyutech.jp>
2019-04-18 21:24:37 +0900

    Fix bug without space behind identifier

--- python/src/lisp03.py
+++ python/src/lisp03.py
@@ -9,7 +9,7 @@ class Reader(object):
             if match != None:
                 detected_token = match.group(0)
                 result.append(detected_token)
-                index += len(detected_token) + 1
+                index += len(detected_token)
             elif src[index] == "(":
                 sub_result = Reader.__make_list(src, index + 1)
                 result.append(sub_result['list'])
@@ -26,6 +26,6 @@ class Reader(object):
         return result['list']
 
 if __name__ == '__main__':
-    source_str = "( A B (C D ) E)"
+    source_str = "( A B (C D ) E (FG H) I)"
     result = Reader.read(source_str)
-    print(result)  # ['A', 'B', ['C', 'D'], 'E']
+    print(result)  # ['A', 'B', ['C', 'D'], 'E', ['FG', 'H', 'I'], 'J']
