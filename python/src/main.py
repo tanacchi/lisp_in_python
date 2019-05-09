a9368da2f45741631dab737973174515239b3cba
tanacchi <q111026d@mail.kyutech.jp>
2019-05-09 16:22:42 +0900

    Make simple adder

--- python/src/main.py
+++ python/src/main.py
@@ -1,9 +1,22 @@
 from reader import Reader
 
+dispatch_table = { 'add': lambda a, b : int(a) + int(b) }
+
+def eval(operand, a, b):
+    return dispatch_table[operand](a, b)
+
+
 def main():
-    source_str  = input("> ")
-    source_list = Reader.read(source_str)
-    print(source_list)
+    while True:
+        source_str  = input("> ")
+        source_list = Reader.read(source_str)
+        print(source_list)
+
+        operand = source_list[0]
+        a = source_list[1]
+        b = source_list[2]
+        result = eval(operand, a, b)
+        print("result: {}\n".format(result))
 
 
 if __name__ == '__main__':
